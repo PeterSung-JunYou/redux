45f5dae525636b2675df763a61de3d0975693e49
유성준(YOU SUNG JUN)/금융사업4그룹/SK <03552@skcc.com>
2020-01-09 14:55:58 +0900

    for gh-pages redux

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                hello redux
+                hello redux and....
             </div>
         );
     }
